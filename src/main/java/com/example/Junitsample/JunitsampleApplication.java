182482f3b28dc121f9421000aa12211664fa71ab
Maha Lakshmi <mahamothilal13@gmail.com>
2022-03-23 12:27:07 +0530

    save

---
+++ src/main/java/com/example/Junitsample/JunitsampleApplication.java
@@ -0,0 +1,19 @@
+package com.example.Junitsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class JunitsampleApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(JunitsampleApplication.class, args);
+		
+	}
+
+}
